1e3f1fb1dda5e673735f16567610caafb724985b
Karan Gohel <karan.gohel@mindinventory.com>
2026-08-11 18:40:57 +0530

    created udpated function with table logic and id logic as well

--- encryptor/encryptorFunction.js
+++ encryptor/encryptorFunction.js
@@ -2,12 +2,19 @@ const Hashids = require("hashids");
 
 const hashids = new Hashids("dont-stall-it-gonna-fall", 12);
 
-function encryptorFunction(dataBaseID) {
-    return hashids.encode(dataBaseID)
+function encryptorFunction(table_id, id) {
+    const valueString = table_id + "0" + id;
+    return hashids.encode(Number(valueString))
 }
-
 function decryptFunction(code) {
-    return hashids.decode(code)
-}
+    const value = String(hashids.decode(code)[0]);
+    console.log(value)
+    let zeroIndex = value.indexOf('0');
 
+    const table_id = value.substring(0, zeroIndex);
+    const id = value.substring(zeroIndex + 1);
+
+    return { "table_id": table_id, "id": id }
+}
+console.log(decryptFunction('gMolEPr3ZYW0'))
 module.exports = { encryptorFunction, decryptFunction };
